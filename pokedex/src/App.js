404bf36f66eef3cd75a62fd8e7e559452f877e23
Poncholo Riego de Dios <poncholoriegodedios@gmail.com>
2025-03-09 02:47:58 +0800

    Card display

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ShowCard from "./Components/ShowCard";
+import PokemonList from "./Components/PokemonList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-wrap items-center h-[100vh] inset-0 justify-center gap-10 p-10 bg-[rgb(255,28,28)]">
+      <div className="bg-white w-full h-full overflow-y-auto flex flex-wrap justify-center items-center gap-4 rounded-2xl no-scrollbar p-4">
+        {PokemonList.map((pokemon) => (
+          <ShowCard
+            key={pokemon.id}
+            name={pokemon.name}
+            type1={pokemon.type1}
+            type2={pokemon.type2}
+          />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ pokedex/src/Components/ShowCard.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+const typeColor = (typing) => {
+  switch (typing) {
+    case "Fire":
+      return <span className="text-orange-900">{typing}</span>;
+    case "Water":
+      return <span className="text-blue-900">{typing}</span>;
+    case "Grass":
+      return <span className="text-green-900">{typing}</span>;
+    case "Electric":
+      return <span className="text-yellow-900">{typing}</span>;
+    case "Poison":
+      return <span className="text-purple-900">{typing}</span>;
+    case "Flying":
+      return <span className="text-indigo-900">{typing}</span>;
+    case "Bug":
+      return <span className="text-lime-900">{typing}</span>;
+    case "Ground":
+      return <span className="text-yellow-900">{typing}</span>;
+    case "Psychic":
+      return <span className="text-pink-900">{typing}</span>;
+    case "Rock":
+      return <span className="text-gray-900">{typing}</span>;
+    case "Ice":
+      return <span className="text-cyan-900">{typing}</span>;
+    case "Dragon":
+      return <span className="text-indigo-900">{typing}</span>;
+    case "Dark":
+      return <span className="text-gray-900">{typing}</span>;
+    case "Steel":
+      return <span className="text-gray-900">{typing}</span>;
+    case "Fairy":
+      return <span className="text-pink-900">{typing}</span>;
+    case "Fighting":
+      return <span className="text-red-900">{typing}</span>;
+    case "Normal":
+      return <span className="text-gray-900">{typing}</span>;
+    default:
+      return <span className="text-gray-900">{typing}</span>;
+  }
+};
+
+export default function ShowCard({ name, type1, type2 }) {
+  return (
+    <div className="flex flex-row gap-4 font-bold">
+      <div className="bg-gray-500 group hover:shadow-lg hover:shadow-white bg-opacity-15 h-60 w-48 rounded-2xl p-2 justify-center flex flex-col items-center transition-all duration-150 gap-2">
+        <div className="w-full h-36 items-center flex justify-center">
+          <img
+            src={`https://projectpokemon.org/images/normal-sprite/${name.toLowerCase()}.gif`}
+            className="w-max h-max bg-transparent rounded-xl"
+          />
+        </div>
+        <p>{name}</p>
+        <p>
+          {typeColor(type1)} {type2 && <span> / {typeColor(type2)}</span>}
+        </p>
+      </div>
+    </div>
+  );
+}
